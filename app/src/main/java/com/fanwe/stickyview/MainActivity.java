2ff578cc0cd7a5dc61441debd6b294269bc5fa0d
zjun <565061763@qq.com>
2018-07-19 17:30:07 +0800

    demo

--- app/src/main/java/com/fanwe/stickyview/MainActivity.java
+++ app/src/main/java/com/fanwe/stickyview/MainActivity.java
@@ -2,13 +2,20 @@ package com.fanwe.stickyview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
     }
 }
