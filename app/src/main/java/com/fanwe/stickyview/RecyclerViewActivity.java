95f0754c395b9b428aa46a1fcc113d0d25c1e306
zjun <565061763@qq.com>
2018-08-27 10:52:34 +0800

    demo

--- app/src/main/java/com/fanwe/stickyview/RecyclerViewActivity.java
+++ app/src/main/java/com/fanwe/stickyview/RecyclerViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
 
@@ -13,7 +14,9 @@ import com.fanwe.lib.stickyview.FStickyLayout;
 import com.fanwe.lib.stickyview.FStickyWrapper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RecyclerViewActivity extends AppCompatActivity
 {
@@ -44,6 +47,8 @@ public class RecyclerViewActivity extends AppCompatActivity
 
     private final FSimpleRecyclerAdapter<DataModel> mAdapter = new FSimpleRecyclerAdapter<DataModel>()
     {
+        private final Map<DataModel, FStickyWrapper> mMapWrapper = new HashMap<>();
+
         @Override
         public int getLayoutId(ViewGroup parent, int viewType)
         {
@@ -54,16 +59,24 @@ public class RecyclerViewActivity extends AppCompatActivity
         public void onBindData(FRecyclerViewHolder<DataModel> holder, int position, DataModel model)
         {
             Button btn = holder.get(R.id.btn);
-            Button btn_sticky = holder.get(R.id.btn_sticky);
-            FStickyWrapper sticky_wrapper = holder.get(R.id.sticky_wrapper);
-
             btn.setText(model.toString());
+
+            ViewGroup fl_content = holder.get(R.id.fl_content);
+            fl_content.removeAllViews();
+
+            FStickyWrapper sticky_wrapper = mMapWrapper.get(model);
+            if (sticky_wrapper == null)
+            {
+                sticky_wrapper = (FStickyWrapper) LayoutInflater.from(getContext()).inflate(R.layout.item_sticky, fl_content, false);
+                mMapWrapper.put(model, sticky_wrapper);
+            }
+            fl_content.addView(sticky_wrapper);
+
+            Button btn_sticky = sticky_wrapper.getSticky().findViewById(R.id.btn_sticky);
             btn_sticky.setText(model.toString());
 
-            if (position == 1)
-                mStickyLayout.addSticky(sticky_wrapper);
+            mStickyLayout.addSticky(sticky_wrapper);
         }
     };
-
 
 }
